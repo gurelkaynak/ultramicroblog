c5a6485c3e1fb02273717109848e0f6b2d8a4e0e
Gürel Kaynak <gurelkaynak@gmail.com>
2015-03-08 20:40:00 +0200

    cors headers settings for dev

--- web/gureka/gureka/settings.py
+++ web/gureka/gureka/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'gureka',
     'ultramicroblog',
@@ -43,6 +44,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -89,4 +91,5 @@ STATIC_URL = '/static/'
     #]
 #}
 
+CORS_ORIGIN_ALLOW_ALL = True
 
